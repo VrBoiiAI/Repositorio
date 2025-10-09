63d5f80f815e93278865083ca7b3a8c70151927d
Arturo <luisa04721@gmail.com>
2025-10-09 15:08:22 -0700

    Update

--- PuntoDeVenta.py
+++ PuntoDeVenta.py
@@ -1,195 +1,160 @@
 import os
 import time
 import csv
+from tabulate import *
 
-inventario = []  #[[Descripcion, Precio, Codigo, Cantidad],..]
-ventaTotal = [0]  #[Total suma, codigos de producto...]
-ticket = [
-  " " + "-" * 75 +
-  "\n|             Descripcion             | precio |   codigo   | cant. | total |"
-  + "\n " + "-" * 75
-]
-lista = ''
+inventario = []  # [[Descripcion, Precio, Codigo, Cantidad],..]
+ventaTotal = [0]  # [Total suma, codigos de producto...]
+columnas = ["Descripcion", "Precio", "Codigo", "Cantidad", "Total"]
+columnas2 = ["Descripcion", "Precio ($)", "Codigo"]
+lista = []
+# Coca Cola 2L Retornable,27.0,CCR2L,0,0
 
 
-def setup(
-):  #Se ejecuta al iniciar el programa, escribe 'productos.txt' en la lista inventario y crea la lista (menu)
-  with open('productos.txt', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for line in csv_reader:
-      inventario.append(line)
+def cls():
+    os.system("cls")
 
-  for i in inventario:
-    i[1] = float(i[1])
-    i[3] = int(i[3])
-    i[-1] = int(i[-1])
 
-  global lista
-  for i in inventario:
-    lista += f'- {i[0]} - ${i[1]} - {i[2]}\n'
+def setup():  # Se ejecuta al iniciar el programa, escribe 'productos.txt' en la lista inventario y crea la lista (menu)
+    global lista
+    with open("productos.txt", "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for line in csv_reader:
+            inventario.append(line)
+
+    for i in inventario:
+        i[1] = float(i[1])
+        i[3] = int(i[3])
+        i[-1] = int(i[-1])
+
+    for i in inventario:
+        lista.append([i[0], i[1], i[2]])
+
+
+def printProds():
+    global lista
+    print(tabulate(lista, headers=columnas2))
 
 
 def escribir():
-  with open('productos.txt', 'w', newline='') as new_file:
-    csv_writer = csv.writer(new_file)
-    csv_writer.writerows(inventario)
+    with open("productos.txt", "w", newline="") as new_file:
+        csv_writer = csv.writer(new_file)
+        csv_writer.writerows(inventario)
 
 
 def printicket():
-  if ticket != [
-      " " + "-" * 75 +
-      "\n|             Descripcion             | precio |   codigo   | cant. | total |"
-      + "\n " + "-" * 75
-  ]:
-    for i in ticket:
-      print(i)
-    print('\n')
+    global ventaTotal
+    data = []
+    if ventaTotal != [0]:
+        for i in ventaTotal[1:]:
+            for j in inventario:
+                if i == j[2]:
+                    data.append([j[0], j[1], j[2], j[-1]])
 
-
-def verify(pregunta, tipo, rango=[]):
-  while True:
-    var = input(pregunta)
-    try:
-      var = tipo(var)
-    except:
-      print('Dato no valido. Intente de nuevo')
-    if type(var) == tipo:
-      if rango == []:
-        break
-      elif var >= rango[0] and var <= rango[1]:
-        break
-      else:
-        print('Valor fuera de las opciones.')
-
-  return var
+        print(tabulate(data, headers=columnas, tablefmt="rounded_grid"))
 
 
 def menu():
-  global ventaTotal, ticket
-  os.system('cls')
-  op = input(
-    '\n[1] Finalizar Venta\n[2] Continuar Venta\n[3] Cancelar Venta\n\nQue accion desea realizar?\n'
-  )
-  if op == '1':
-    for i in inventario:
-      if i[2] in ventaTotal:
-        i[-1] = 0
-    printicket()
-    print(f'Total {"."*(66-len(str(ventaTotal[0])))}  ${ventaTotal[0]}')
-    ventaTotal = [0]
-    ticket = [
-      " " + "-" * 75 +
-      "\n|             Descripcion             | precio |   codigo   | cant. | total |"
-      + "\n " + "-" * 75
-    ]
-    input('Enter para realizar otra venta...')
-    escribir()
-    venta()
-  elif op == '2':
-    venta()
-  elif op == '3':
-    os.system('cls')
+    global ventaTotal
+    cls()
     op = input(
-      '\n[1] Cancelar Venta\n[2] Eliminar Producto\n\nQue accion desea realizar?\n'
+        "\n[1] Finalizar Venta\n[2] Continuar Venta\n[3] Cancelar Venta\n\nQue accion desea realizar?\n"
     )
-    if op == '1':
-      for i in inventario:
-        if i[2] in ventaTotal:
-          i[-1] = 0
-
-      ventaTotal = [0]
-      ticket = [
-        " " + "-" * 75 +
-        "\n|             Descripcion             | precio |   codigo   | cant. | total |"
-        + "\n " + "-" * 75
-      ]
-      input('Enter para realizar otra venta...')
-      venta()
-
-    elif op == '2':
-      printicket()
-      code = input(
-        'Ingrese el codigo del producto que desea eliminar: ').upper()
-      if code in ventaTotal:
-        ventaTotal.remove(code)
+    if op == "1":
+        printicket()
+        print(f"Total", "." * 30, "$", ventaTotal[0])
         for i in inventario:
-          if i[2] == code:
-            i[-1] = 0
-            ventaTotal[0] -= i[1] * i[-2]
-        for i in ticket:
-          if code in i:
-            index = ticket.index(i)
-            ticket.pop(index)
-            ticket.pop(index)
+            if i[2] in ventaTotal:
+                i[-1] = 0
+        ventaTotal = [0]
+        input("Enter para realizar otra venta...")
+        escribir()
+        venta()
+    elif op == "2":
+        venta()
+    elif op == "3":
+        cls()
+        op = input(
+            "\n[1] Cancelar Venta\n[2] Eliminar Producto\n\nQue accion desea realizar?\n"
+        )
+        if op == "1":
+            for i in inventario:
+                if i[2] in ventaTotal:
+                    i[-2] += i[-1]
+                    i[-1] = 0
+            ventaTotal = [0]
+            input("Enter para realizar otra venta...")
+            venta()
 
-      else:
-        print('El producto no está en la lista...')
+        elif op == "2":
+            printicket()
+            code = input("Ingrese el codigo del producto que desea eliminar: ").upper()
+            if code in ventaTotal:
+                ventaTotal.remove(code)
+                for i in inventario:
+                    if i[2] == code:
+                        ventaTotal[0] -= i[1] * i[-1]
+                        i[-1] = 0
+                        i[-2] += i[-1]
+
+            else:
+                print("El producto no está en la lista...")
+                menu()
+            venta()
+
+        else:
+            input("Opcion no valida. Intente de nuevo.")
         menu()
-      venta()
 
-  else:
-    pass
+    else:
+        input("Opcion no valida. Intente de nuevo.")
+        menu()
 
 
 def venta():
-  os.system('cls')
-  printicket()
-  print(lista)
-  code = input(
-    "\nIngrese el codigo del producto ('M' para ir al menú): ").upper()
-  conf = False
-  for i in inventario:
-    if code == i[2]:
-      #print('\n')
-      if not (i[-2] <= 0):
-        i[-1] += 1
-        i[-2] -= 1
-        if i[2] not in ventaTotal:
-          ticket.append(
-            f'| {i[0]}{" "*(36-len(i[0]))}| {i[1]}{" "*(7-len(str(i[1])))}| {i[2]}{" "*(11-len(i[2]))}| {i[-1]}{" "*(6-len(str(i[-1])))}| {i[1]*i[4]}{" "*(6-len(str(i[1]*i[4])))}|'
-          )
-          ticket.append(" " + "-" * 75)
-          ventaTotal.append(i[2])
-          ventaTotal[0] += i[1]
-          conf = True
+    cls()
+    printicket()
+    printProds()
+    code = input("\nIngrese el codigo del producto ('M' para ir al menú): ").upper()
+    conf = False
+    for i in inventario:
+        if code == i[2]:
+            if i[-2] > 0:
+                i[-1] += 1
+                i[-2] -= 1
+                if i[2] not in ventaTotal:
+                    ventaTotal.append(i[2])
+                    ventaTotal[0] += i[1]
+                    conf = True
 
-        else:
-          for j in ticket:
-            if i[0] in j:
-              ventaTotal[0] += i[1]
-              ind = ticket.index(j)
-              ticket.pop(ind)
-              ticket.pop(ind)
-              ticket.append(
-                f'| {i[0]}{" "*(36-len(i[0]))}| {i[1]}{" "*(7-len(str(i[1])))}| {i[2]}{" "*(11-len(i[2]))}| {i[-1]}{" "*(6-len(str(i[-1])))}| {i[1]*i[4]}{" "*(6-len(str(i[1]*i[4])))}|'
-              )
-              ticket.append(" " + "-" * 75)
-              conf = True
+                else:
+                    ventaTotal[0] += i[1]
+                    conf = True
 
+                venta()
+
+            else:
+                os.system("cls")
+                print("No hay mas existencia de este producto :(")
+                time.sleep(2)
+                venta()
+
+    if code == "M":
+        conf = True
+        menu()
+
+    if conf == False:
+        for i in range(2):
+            os.system("cls")
+            print("No existe el producto.")
+            time.sleep(0.4)
+            os.system("cls")
+            print("No existe el producto..")
+            time.sleep(0.4)
+            os.system("cls")
+            print("No existe el producto...")
+            time.sleep(0.4)
         venta()
-
-      else:
-        os.system("cls")
-        print("No hay mas existencia de este producto :(")
-        time.sleep(2)
-        venta()
-
-  if code == 'm' or code == 'M':
-    conf = True
-    menu()
-
-  if conf == False:
-    for i in range(2):
-      os.system("cls")
-      print("No existe el producto.")
-      time.sleep(.4)
-      os.system("cls")
-      print("No existe el producto..")
-      time.sleep(.4)
-      os.system("cls")
-      print("No existe el producto...")
-      time.sleep(.4)
-    venta()
 
 
 setup()
